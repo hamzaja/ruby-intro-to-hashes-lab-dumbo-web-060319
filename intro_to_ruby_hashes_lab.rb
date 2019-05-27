214e4044ca86ce15dcbec6e55632fdcdc30c8336
HAMZA Javed <hamzajaved71@gmail.com>
2019-05-27 22:03:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ monopoly =  {
   :railroads=>{
      :pieces=>4,
       :names=>{:reading_railroad=>{},
-      :pennsylvania_railroad=>
+      :pennsylvania_railroad=>{}
         
       },
       
