05be655ff22429c503c0fa5e15cd919d9cfc722b
HAMZA Javed <hamzajaved71@gmail.com>
2019-05-27 22:02:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,8 @@ def monopoly_with_third_tier
 monopoly =  {
   :railroads=>{
      :pieces=>4,
-      :names=>{:reading_railroad=>{}
+      :names=>{:reading_railroad=>{},
+      pennsylvania_railroad=>
         
       },
       
